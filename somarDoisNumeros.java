9539c6a548be31c519dd7ed6542eb289424184d4
Karen Satiro <124855248+kahsatiro@users.noreply.github.com>
2025-09-05 01:34:50 -0300

    Add files via upload

---
+++ somarDoisNumeros.java
@@ -0,0 +1,37 @@
+package unidade1;
+
+import java.util.*;
+
+class somarDoisNumeros {
+	
+	public static Scanner sc = new Scanner(System.in);
+	
+	public static void main (String args[]){
+		//Declaracao de variaveis
+		int num1, num2, soma;
+		//Leituras
+		System.out.println("Digite um número:");
+		num1 = sc.nextInt();
+		System.out.println("Digite outro número:");
+		num2 = sc.nextInt();
+		//Somar
+		soma = num1 + num2;
+		//Mostrar na tela
+		System.out.println("Soma:" + soma);
+		}
+
+	public static Scanner getSc() {
+		return sc;
+	}
+
+	public static void setSc(Scanner sc) {
+		somarDoisNumeros.sc = sc;
+	}
+
+	@Override
+	public String toString() {
+		return "somarDoisNumeros [getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()="
+				+ super.toString() + "]";
+	}
+
+}
